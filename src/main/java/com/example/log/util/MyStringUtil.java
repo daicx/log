6bc7be6a0313f151d34b925c72cc400080c71359
dcx <807404400@qq.com>
2020-05-31 13:16:37 +0800

    12

---
+++ src/main/java/com/example/log/util/MyStringUtil.java
@@ -0,0 +1,23 @@
+package com.example.log.util;
+
+public class MyStringUtil {
+    public static void main(String[] args) {
+        double longitude=37.09024;
+        double latitude=-95.712891;
+//        double longitude=116.40387397;
+//        double latitude=39.91488908;
+
+        //-95.712891<br>当前的经度：37.09024
+//        GeoTrans.init(true,true,true,true);
+//        Admin admin = GeoTrans.determineAdmin(longitude, latitude, null, true);
+//        System.out.println(admin);
+//        String city = admin.city();
+//        String district = admin.district();
+//        String country = admin.country();
+//        Admin admin1 = admin.toShort();
+//
+//        Location center = admin.center();
+//        System.out.println(city);
+//        System.out.println(center);
+    }
+}
